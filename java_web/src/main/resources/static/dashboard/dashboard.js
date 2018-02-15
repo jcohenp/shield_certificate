9c099336dfb895061168bc3f9ad5d2f8fd9bd80a
Julien COHEN <julien.cohen@engie.com>
2018-02-15 13:35:16 +0100

    [DASHBOARD] - include angular ui and implement datepicker in the modal

--- java_web/src/main/resources/static/dashboard/dashboard.js
+++ java_web/src/main/resources/static/dashboard/dashboard.js
@@ -1,13 +1,76 @@
 'use strict';
 
-angular.module('myApp.dashboard', ['ngRoute'])
+angular.module('myApp.dashboard', ['ngRoute', 'ui.bootstrap'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {
     templateUrl: 'dashboard/dashboard.html',
     controller: DashboardCtrl,
 		resolve: DashboardCtrl.resolve
   });
-}]);
+}])
+.controller('CreateCertificateCtrl', function ($scope, $http) {
+
+	// datePicker ANGULAR UI
+    $scope.today = function() {
+        $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.clear = function() {
+        $scope.dt = null;
+    };
+
+    $scope.dateOptions = {
+        formatYear: 'yy',
+        maxDate: new Date(2020, 5, 22),
+        minDate: new Date(),
+        startingDay: 1
+    };
+
+    $scope.open1 = function() {
+    	console.log("toto");
+        $scope.popup1.opened = true;
+    };
+
+
+    $scope.setDate = function(year, month, day) {
+        $scope.dt = new Date(year, month, day);
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
+    $scope.altInputFormats = ['M!/d!/yyyy'];
+
+    $scope.popup1 = {
+        opened: false
+    };
+
+    // SEND MODAL FOR CERTIFICATE CREATION
+
+
+    $scope.serverResponse = '';
+
+    var setResponse = function(res) {
+        $rootScope.authenticated = isAuthenticated;
+        $scope.serverResponse = res;
+    };
+
+    $scope.CreateCertificate = function() {
+        $http({
+            headers: authService.createAuthorizationTokenHeader(),
+            method: 'POST',
+            url: 'api/certificat/create_certificat'
+        })
+            .then(function(res) {
+                setResponse(res, true);
+            })
+            .catch(function(response) {
+                setResponse(response, false);
+            });
+    };
+
+
+});
 
 function DashboardCtrl($scope, $rootScope, $http, isAuthenticated, authService) {
 	$rootScope.authenticated = isAuthenticated;
@@ -41,10 +104,7 @@ function DashboardCtrl($scope, $rootScope, $http, isAuthenticated, authService) 
 	}
 
 	$scope.showModal = function(event) {
-        $("#DataCertificate").find("input#userName").val(event.currentTarget.attributes["data-userName"].value);&
-	};
-	$scope.CreateCertificate = function(event) {
-
+        $("#DataCertificate").find("input#userName").val(event.currentTarget.attributes["data-userName"].value);
 	};
 }
 DashboardCtrl.resolve = {
