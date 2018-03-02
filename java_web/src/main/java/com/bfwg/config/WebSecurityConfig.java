9af4c6ed3b11b812e6efd6e540acda8c10e487d8
Marius Tanawa Tsamo <tanawa_m@epita.fr>
2018-03-02 10:47:38 +0100

    Fix the interface

--- java_web/src/main/java/com/bfwg/config/WebSecurityConfig.java
+++ java_web/src/main/java/com/bfwg/config/WebSecurityConfig.java
@@ -80,6 +80,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         "/favicon.ico",
                         "/**/*.html",
                         "/**/*.css",
+                        "/**/*.png",
                         "/**/*.js"
                 ).permitAll()
                 .antMatchers("/auth/**").permitAll()
